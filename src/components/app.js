a25811d46887ba3e6497550ac0bbecce0c6b77ab
Ronald Coe Jr <rcoe@miosys.org>
2019-07-27 17:08:23 -0400

    fix div problem

--- src/components/app.js
+++ src/components/app.js
@@ -7,11 +7,12 @@ class App extends React.Component {
         this.state = {}
     }
     render() {
-        return ( "test" )
+        return ( <div className="container"> <input className="btn btn-primary" type="submit" value="Alert" onClick={() => {alert("Alert button onClick");}}/> 
+		</div> )
     }
 }
 
 const app = document.getElementById('app');
 if(app){
-    ReactDOM.render(&ltApp/>, app);
+    ReactDOM.render(<App/>, app);
 }
